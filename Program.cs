c74036303a14827aba469bf4f29d89fd90c4a63e
Tony Davis <anthony79@mail.usf.edu>
2020-01-22 18:25:20 -0500

    Commit the WriteLine Change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_Learning_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
