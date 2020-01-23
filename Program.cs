0874c16256495cb4d3e078ea9a683396e4d460ef
Tony Davis <anthony79@mail.usf.edu>
2020-01-22 22:57:36 -0500

    Commit comment additions

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ namespace My_Learning_Demo
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // Display the text "Hello World".
             Console.WriteLine("Hello World!");
+            // Display the text This is ISM 4300.
+            Console.WriteLine("This is ISM 4300");
         }
     }
 }
